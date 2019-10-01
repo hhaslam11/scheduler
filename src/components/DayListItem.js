aad8d0fa0cd1056ec7646cfc0fde3edf2b8c3642
hhaslam11 <kalebmarc@hotmail.com>
2019-09-30 19:29:07 -0700

    add DayListItem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,42 @@
+import React from "react";
+import classnames from"classnames";
+
+//styles
+import "./DayListItem.scss";
+
+
+export default function DayListItem(props) {
+  
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+  
+  return (
+    <li 
+      className={dayClass} 
+      onClick={() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots}</h3>
+    </li>
+  )
+}
+
+//==
+// export default function Button(props) {
+//   const buttonClass = classnames("button", {
+//     "button--confirm": props.confirm,
+//     "button--danger": props.danger
+//   });
+
+//   return (
+//     <button
+//       className={buttonClass}
+//       onClick={props.onClick}
+//       disabled={props.disabled}
+//     >
+//       {props.children}
+//     </button>
+//   );
+// }
