97edc92ae6dd97e7031ff9694374a5b12261a06a
hhaslam11 <kalebmarc@hotmail.com>
2019-10-01 17:09:36 -0700

    add appointment header, empty, and show

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+export default function Appointment(props) {
+  return <article className="appointment">Appointment</article>;
+}
--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,10 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -127,3 +131,26 @@ storiesOf("InterviewerList", module)
       setInterviewer={action("setInterviewer")}
     />
   ));
+
+storiesOf("Appointment", module)
+    .addParameters({
+      backgrounds: [{ name: "white", value: "#fff", default: true }]
+    })
+    .add("Appointment", () => (
+      <Appointment />
+    ))
+    .add("Appointment with time", () => (
+      <Appointment time="12pm" />
+    ))
+    .add("Header", () => (
+      <Header time="12pm" />
+    ))
+    .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+    .add("Show", () => (
+      <Show 
+        student="Lydia Miller-Jones"
+        interviewer={interviewers[0]}
+        onEdit={action("Edit")}
+        onDelete={action("Delete")}
+      />
+    ));
