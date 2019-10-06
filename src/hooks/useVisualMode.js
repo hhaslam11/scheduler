e6c6221809070606d43466b09124f9b25b7e2922
hhaslam11 <kalebmarc@hotmail.com>
2019-10-05 19:07:50 -0700

    remove unused console.logs

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -14,7 +14,7 @@ export default function useVisualMode(initial) {
     if (history.length === 1) return;
 
     const newHistory = history.slice(0, history.length - 1);
-    console.log('newHistory:', newHistory);
+    
     setHistory(newHistory);
     setMode(newHistory[newHistory.length - 1]);
   };
