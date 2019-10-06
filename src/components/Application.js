6e220a12e067a84c454310bdebbd5bc6c76e0022
hhaslam11 <kalebmarc@hotmail.com>
2019-10-05 19:07:01 -0700

    move application logic to a seperate hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,146 +1,19 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 import "components/Application.scss";
 import "components/Application";
+import useApplicationData from "hooks/useApplicationData";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "../helpers/selectors";
 
 export default function Application() {
-
-  /**
-   * ~~ State contains the following information ~~
-   * 
-   * day: string | the day that is selected.
-   * days: Array of objects =>
-   *    {
-   *     id: 1,
-   *     name: "Monday",
-   *     appointments: [1,2,3,4,5],
-   *     interviewers: [2,3,5,7,10],
-   *     spots: 3
-   *    }
-   * appointments: object of objects => 
-   *    {
-   *      1: {
-   *        id: 1,
-   *        time: "12pm",
-   *        interview: { student: "Archie Cohen", interviewer: 10 }
-   *        }
-   *      ...
-   *    }
-   * interviewers: object of objects => 
-   *    {
-   *      1: {
-   *        id: 1,
-   *        name: "Sylvia Palmer",
-   *        avatar: "https://i.imgur.com/LpaY82x.png"
-   *        }
-   *    }
-   */
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });  
-
-  //Changes day in the state || "Monday", "Tuesday", etc..
-  const setDay = day => setState(prev => {
-    return ({ ...prev, day});
-  });
-
-  /**
-   * Create a new appointment and puts it in state
-   * @param {number} id appointment id
-   * @param {object} interview { student: "Archie Cohen", interviewer: 10 }
-   */
-  function bookInterview(id, interview) {
-    
-    /*
-     * id: state.appointments[id].id
-     * time: state.appointments[id].time,
-     * interview: interview
-     */
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-
-    /*
-     * All of state.appointments
-     * and replacing appointments[id] with 
-     * the new appointment created above
-     * that has the new interview
-     */
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios.put(`/api/appointments/${id}`, appointment)
-      .then(() => {
-        //Replace state with a copy that has the new appointments object in it
-        setState(prev => ({...prev, appointments}));
-      });
-  }  
-
-  /**
-   * Sets appointment info to null
-   * @param {number} id appointment id
-   */
-  function cancelInterview(id) {
-    if (state.appointments[id]) state.appointments[id] = null;
-
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios.delete(`/api/appointments/${id}`)
-      .then(() => {
-        console.log('This should not run');
-        //Replace state with a copy that has the new appointments object in it
-        setState(prev => ({...prev, appointments}));
-      });
-    
-  }
-
-  //This should only run once, when the app is first initialized :)
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ])
-      .then(res => {
-        // Data structure from the api is the same as described above the useState call
-        const days = res[0].data;
-        const appointments = res[1].data;
-        const interviewers = res[2].data;
-
-        /*
-         * Puts the data from the api into state
-         *
-         * prev probably doesnt need to be used here
-         * (this only runs once, and state should be empty at this point anyways)
-         * but I'll keep it here anyways, just in case~
-         */
-        setState(prev => {
-          return {
-            ...prev,
-            days,
-            appointments,
-            interviewers
-          }
-        });
-      });
-  }, []);
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   return (
     <main className="layout">
