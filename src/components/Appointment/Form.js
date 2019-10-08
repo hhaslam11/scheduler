df78a912a14e9987f7abb12bbf90576f7f6d9a24
hhaslam11 <kalebmarc@hotmail.com>
2019-10-07 21:45:19 -0700

    add lots of integration tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -10,7 +10,7 @@ import "./styles.scss";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-  
+  const [error, setError] = useState("");
   function reset() {
     setName("");
     setInterviewer(null);
@@ -21,6 +21,17 @@ export default function Form(props) {
     props.onCancel();
   }
 
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    setError("");
+    props.onSave(name, interviewer);
+  }
+
+  
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -32,19 +43,20 @@ export default function Form(props) {
             type="text"
             placeholder={"Enter Student Name"}
             value={name || ""}
-            onInput={event => setName(event.target.value) /*TODO is re-rendering the form each time a letter is typed bad practice? */}
+            onChange={event => setName(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
-
+        <section className="appointment__validation">{error}</section>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={() => props.onSave(name, interviewer)} confirm>Save</Button>
+          <Button onClick={() => validate()} confirm>Save</Button>
         </section>
       </section>
     </main>
-
   );
+  
 }
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import InterviewerListItem from "./InterviewerListItem";
 
 import "./InterviewerList.scss";
@@ -7,9 +8,17 @@ import "./InterviewerList.scss";
  * @param {object} props {interviewers (object from top level -application- state), value (interviewer id) onChange (function to setInterviewer)} 
  */
 export default function InterviewerList(props) {
+  //verify corrent prop value types
+  InterviewerList.propTypes = {
+    value: PropTypes.number,
+    onChange: PropTypes.func.isRequired
+  };
+
+  
   const interviewers = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
+        key={interviewer.id}
         id={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
